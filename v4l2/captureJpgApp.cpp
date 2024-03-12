de98993e52d46cca183e4fb0ad04db5a5496acec
LitchiCheng <974782852@qq.com>
2024-03-12 22:17:18 +0800

    feat:add yolov5\v4l2

---
+++ v4l2/captureJpgApp.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "v4l2CapPicTool.h"
+#include "timecost.h"
+
+int main(int argc, char const *argv[])
+{
+    v4l2CapPicTool vt("/dev/video10", 1080, 960, "jpg");
+    vt.init();
+    while (1)
+    {
+        usleep(100000);
+        {
+            timecost tmp("capture");
+            vt.capture();
+        }
+            
+        {
+            timecost tmp("save");
+            vt.save("./test.jpg"); 
+        }
+    }
+    return 0;
+}
